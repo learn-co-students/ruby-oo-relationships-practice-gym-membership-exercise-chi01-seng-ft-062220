49593eb6b250189c5621a4fa1256692c4a4c5689
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-06-29 19:11:05 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,36 @@
 class Gym
+
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select { |contract| contract.gym == self }
+  end
+
+  def lifters_at_gym
+    my_members = Membership.all.select { |contract| contract.gym == self }
+    my_members.map { |membership| membership.lifter}
+  end
+
+  def names_of_lifters
+    members = Membership.all.select { |contract| contract.gym == self} 
+    my_lifters = members.map  { |membership| membership.lifter}
+    my_lifters.map { |lifter| lifter.name} 
+    # my_lifters.select { |membership| member.name}
+  end
+
+  def lift_total
+    my_members = Membership.all.select { |contract| contract.gym == self }
+    my_lifters = my_members.map  { |membership| membership.lifter}
+    my_lifters.sum { |member| member.lift_total}
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,40 @@
 class Lifter
+
+  @@all = []
   attr_reader :name, :lift_total
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+  def self.all 
+    @@all 
+  end
+
+  def memberships
+    Membership.all.select { |contract| contract.lifter == self }
+  end
+
+  def gyms
+    my_gyms = Membership.all.select { |contract| contract.lifter == self }
+    my_gyms.map { |membership| membership.gym}
+  end
+
+  def total_cost
+    my_gyms = Membership.all.select { |contract| contract.lifter == self }
+    my_gyms.sum { |membership| membership.cost }
+  end
+
+  def signup(gym, cost)
+    Membership.new(cost, gym, self)
+  end
+
+  def self.lift_total
+    Lifter.all.sum { |lifter| lifter.lift_total} / Lifter.all.count
+  end
+
+
 end
