0766a9da5d5bc27c4b9470b7bf3cccdf91a4f046
foxten <tenaysiafox@gmail.com>
2020-06-29 19:31:48 -0500

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,42 @@
 class Lifter
-  attr_reader :name, :lift_total
+  attr_reader :name, :lift_total, :gym_name
+  @@all = []
 
+  # Get a list of all lifters
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  # Get a list of all the memberships that a specific lifter has
+  def all_memberships
+    Membership.all.select {|member_info| member_info.member_name == self}
+  end
+  
+  # Get a list of all the gyms that a specific lifter has memberships to
+  def specific_gyms
+    Membership.all.select {|member_info| member_info.member_name == self}.map{|member_info| member_info.gym_name.name}
+  end
+
+  # Get the average lift total of all lifters
+  def self.average_total
+    lifts = self.all.map {|info| info.lift_total}
+    lifts.sum/lifts.count
+  end
+
+  # Get the total cost of a specific lifter's gym memberships
+  def total_cost
+    Membership.all.select {|member_info| member_info.member_name == self}.map{|member_info| member_info.cost}.sum
+  end
+
+  # Given a gym and a membership cost, sign a specific lifter up for a new gym
+  def new_member(cost, gym)
+    Membership.new(cost, self, gym)
   end
 end
+#worked
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
   attr_reader :cost
+  attr_accessor :member_name, :gym_name
+  @@all = []
 
-  def initialize(cost)
+  # Get a list of all memberships
+  def initialize(cost,member_name,gym_name)
     @cost = cost
+    @gym_name = gym_name
+    @member_name = member_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
+#worked
