6f166f139a0c2bb708f7d4408dc9b24a1a50e4ab
Sam M <sammontalvojr@gmail.com>
2020-06-29 22:53:59 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def all_memberships
+    Membership.all.select {|sub| sub.gym == self}
+  end
+
+  def self.lifters_for(gym)
+    result = Membership.all.select {|sub| sub.gym == gym}
+    result.map{|sub| sub.lifter.name}
+  end
+
+  def all_lifters
+    self.all_memberships.map {|sub| sub.lifter.name}
+  end
+
+  def lift_total
+    subs = self.all_memberships
+    subs.uniq!
+    subs.inject(0) {|sum, sub| sum += sub.lifter.lift_total}
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,16 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+arnold = Lifter.new("Arnold", 220)
+golds = Gym.new("Golds")
+gold_membership = Membership.new(80, arnold, golds)
+planet_fitness = Gym.new("Planet Fitness")
+black_card_membership = Membership.new(15, arnold, planet_fitness)
+la_fitness = Gym.new("LA Fitness")
+la_fit_membership = Membership.new(40, arnold, la_fitness)
+sam = Lifter.new("Sam", 120)
+black_card_founder = Membership.new(20, sam, planet_fitness)
+sam.new_membership(40, la_fitness)
 
 binding.pry
 
